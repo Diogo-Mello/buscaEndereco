08928ae58f03df05a0876ac61714bb3b322d0262
Diogo-Mello <diogomellodacrus1108@gmail.com>
2022-10-29 15:37:00 -0300

    Adição tabela com mapa

--- js/functions.js
+++ js/functions.js
@@ -20,7 +20,7 @@ async function buscarCep() {
         inputCep.value = ''
 
         const resultado = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-        const dados = await resultado.json()
+        var dados = await resultado.json()
 
         if (dados.erro) {
 
@@ -31,9 +31,11 @@ async function buscarCep() {
         else {
             indexResult.innerHTML = `ENCONTRADO`
 
-        inserirResultados(dados)
+            verificarNulos(dados)
+
+            inserirResultados(dados, cep)
         }
-        
+
     }
 }
 
@@ -45,39 +47,114 @@ function verificarNumero(n) {
 
 // Função que inseri resultados caso a busca de certo
 
-function inserirResultados(dados) {
+function inserirResultados(dados, cep) {
     indexResultado.innerHTML = `
-    <p>Bairro: <span id="bairro"></span></p>
-    <p>CEP: <span id="cep"></span></p>
-    <p>Complemento: <span id="complemento"></span></p>
-    <p>DDD: <span id="ddd"></span></p>
-    <p>GIA: <span id="gia"></span></p>
-    <p>IBGE: <span id="ibge"></span></p>
-    <p>Localidade: <span id="localidade"></span></p>
-    <p>Logradouro: <span id="logradouro"></span></p>
-    <p>Siafi: <span id="siafi"></span></p>
-    <p>UF: <span id="uf"></span></p>
+    <table>
+                <tr>
+                    <th colspan="3">ENDEREÇO:</th>
+                </tr>
+
+                <tr>
+                    <td colspan="3">
+                        <p>Logradouro:</p>
+                        <span>${dados.logradouro}</span>
+                    </td>
+                </tr>
+                
+
+                <tr>
+                    <td colspan="3">
+                        <p>Bairro:</p>
+                        <span>${dados.bairro}</span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="3">
+                        <p>Complemento:</p>
+                        <span>${dados.complemento}</span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="3">
+                        <p>Cidade:</p>
+                        <span>${dados.localidade}</span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <p>DDD:</p>
+                        <span>${dados.ddd}</span>
+                    </td>
+
+                    <td>
+                        <p>UF:</p>
+                        <span>${dados.uf}</span>
+                    </td>
+
+                    <td>
+                        <p>CEP:</p>
+                        <span>${dados.cep}</span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <p>IBGE:</p>
+                        <span>${dados.ibge}</span>
+                    </td>
+
+                    <td>
+                        <p>GIA:</p>
+                        <span>${dados.gia}</span>
+                    </td>
+
+                    <td>
+                        <p>SIAFI:</p>
+                        <span>${dados.siafi}</span>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td colspan="3">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117168.74493186254!2d-47.70936226379985!3d-23.40545724992264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5e6bcb072354f%3A0x420530adb99e9506!2sIper%C3%B3%20-%20SP%2C%20${cep}!5e0!3m2!1spt-BR!2sbr!4v1667067120276!5m2!1spt-BR!2sbr" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </td>
+                </tr>
+            </table>
     `
+}
 
-    var indexBairro = document.getElementById('bairro');
-    var indexCep = document.getElementById('cep');
-    var indexComplemento = document.getElementById('complemento');
-    var indexDDD = document.getElementById('ddd');
-    var indexGIA = document.getElementById('gia');
-    var indexIBGE = document.getElementById('ibge');
-    var indexLocalidade = document.getElementById('localidade');
-    var indexLogradouro = document.getElementById('logradouro');
-    var indexSiafi = document.getElementById('siafi');
-    var indexUf = document.getElementById('uf');
-
-    indexBairro.innerHTML = `${dados.bairro}`
-    indexCep.innerHTML = `${dados.cep}`
-    indexComplemento.innerHTML = `${dados.complemento}`
-    indexDDD.innerHTML = `${dados.ddd}`
-    indexGIA.innerHTML = `${dados.gia}`
-    indexIBGE.innerHTML = `${dados.ibge}`
-    indexLocalidade.innerHTML = `${dados.localidade}`
-    indexLogradouro.innerHTML = `${dados.logradouro}`
-    indexSiafi.innerHTML = `${dados.siafi}`
-    indexUf.innerHTML = `${dados.uf}`
+function verificarNulos(dados) {
+    if (dados.logradouro == '') {
+        dados.logradouro = 'nulo'
+    }
+    if (dados.bairro == '') {
+        dados.bairro = 'nulo'
+    }
+    if (dados.complemento == '') {
+        dados.complemento = 'nulo'
+    }
+    if (dados.localidade == '') {
+        dados.localidade = 'nulo'
+    }
+    if (dados.ddd == '') {
+        dados.ddd = 'nulo'
+    }
+    if (dados.uf == '') {
+        dados.uf = 'nulo'
+    }
+    if (dados.cep == '') {
+        dados.cep = 'nulo'
+    }
+    if (dados.ibge == '') {
+        dados.ibge = 'nulo'
+    }
+    if (dados.gia == '') {
+        dados.gia = 'nulo'
+    }
+    if (dados.siafi == '') {
+        dados.siafi = 'nulo'
+    }
 }
